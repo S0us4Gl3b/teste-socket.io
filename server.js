211f197b343c91477e5b751a96f732de5d7e02b1
S0us4Gl3b <glebsonsalmeida@gmail.com>
2024-07-09 15:54:33 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const path = require('path')
+const { Socket } = require('socket.io')
+
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
+
+var numeroConexoes = 0
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'public'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+
+app.use('/', (req, res) => {
+    res.render('index.html')
+})
+
+let messages = []
+
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`)
+
+    socket.emit('previousMessages', messages)
+
+    socket.on('sendMessage', data => {
+        messages.push(data)
+        socket.broadcast.emit('receivedMessage', data)
+    })
+
+    numeroConexoes = numeroConexoes + 1
+    //console.log(`Numero de conexoes: ${numeroConexoes}`)
+})
+
+server.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000')
+})
